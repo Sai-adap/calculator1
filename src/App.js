e219d0943bdc0930f00ea6f96e5e26993f8c1d6c
RAJ KUMAR <saikumaradapa20@gmail.com>
2022-11-01 15:16:15 +0530

    MY CALCULATOR

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React,{useState} from "react"
 import './App.css';
-
 function App() {
+  const [input,setinput]=useState(0)
+  const handler=(e)=>{
+    setinput(e.target.value)
+  }
+  const result=()=>{
+    setinput(eval(input))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <div className="main">
+        <center>
+          <h1>CALCULATOR</h1>
+          <input type="text" value={input} name="input" onChange={handler} />
+          <button onClick={()=>{setinput('')}}>C</button>
+          <div>
+            <button onClick={()=>{setinput(input+"1")}}>1</button>
+            <button onClick={()=>{setinput(input+"2")}}>2</button>
+            <button onClick={()=>{setinput(input+"3")}}>3</button>
+            <button onClick={()=>{setinput(input+"/")}}>/</button>
+          </div>
+          <div>
+            <button onClick={()=>{setinput(input+"4")}}>4</button>
+            <button onClick={()=>{setinput(input+"5")}}>5</button>
+            <button onClick={()=>{setinput(input+"6")}}>6</button>
+            <button onClick={()=>{setinput(input+"-")}}>-</button>
+          </div>
+          <div>
+            <button onClick={()=>{setinput(input+"7")}}>7</button>
+            <button onClick={()=>{setinput(input+"8")}}>8</button>
+            <button onClick={()=>{setinput(input+"9")}}>9</button>
+            <button onClick={()=>{setinput(input+"*")}}>*</button>
+          </div>
+          <div>
+          <button onClick={()=>{setinput(input+".")}}>.</button>
+          <button onClick={()=>{setinput(input+"0")}}>0</button>
+          <button onClick={()=>{setinput(input+"+")}}>+</button>
+          <button onClick={result}>=</button>
+          </div>
+
+        </center>
+      </div>
+      
     </div>
+    </>
   );
 }
 
